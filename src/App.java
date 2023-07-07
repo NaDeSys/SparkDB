b4749b1e362818d7574a1a557d5f88ac4ebeb9cc
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-07 21:20:08 +0300

    Examples

--- src/App.java
+++ src/App.java
@@ -14,12 +14,32 @@ public class App {
          * Therefore, db.txt is in the parent directory.
          */
         Table.readFromFile("../db.txt");
+
         /* Get all tomato prices */
+
         ArrayList<String> TomatoPrices = Table.get(new HashMap<>() {{
             put("product", "tomato");
         }}, "price");
         System.out.println("Tomato Price list: " + TomatoPrices);
+
         /* Get all rows where the month is 5 */
-        
+
+        ArrayList<HashMap<String, String>> Rows = Table.getRows(new HashMap<>() {{
+            put("month", "5");
+        }});
+        System.out.println(Rows);
+
+        /* Get all rows where product is tomato and month is lower than or equal 7 */
+
+        ArrayList<HashMap<String, String>> RowsV2 = new ArrayList<>();
+        for(int Month = 1; Month <= 7; Month ++) {
+            final int MonthFinal = Month; // Due to a stupid convention by Oracle, we are forced to do this
+
+            RowsV2.addAll(Table.getRows(new HashMap<>() {{
+                put("product", "tomato");
+                put("month", String.valueOf(MonthFinal));
+            }}));
+        }
+        System.out.println(RowsV2);
     }
 }
